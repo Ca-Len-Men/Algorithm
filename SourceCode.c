3fb3d328220a708c23b36be29a803fcfb8ca3217
Ca-Len-Men <88229844+Ca-Len-Men@users.noreply.github.com>
2021-11-15 17:30:15 +0700

    Update SourceCode.c

--- SourceCode.c
+++ SourceCode.c
@@ -1,3 +1,34 @@
+/*Hướng dẫn
+Input : 1 2 3 4 3 3 1
+Output: 1 2 3 5 1 2 1
+
+Giải thích Output :
+- Ở phần tử thứ 3 ( mang giá trị 4 ) : bên trái có dãy con 1 2 3 tăng dần, bên phải có dãy con 3 giảm dần ( tất cả nhỏ hơn 4 )
+- Như vậy, độ dốc của phần tử thứ 3 ( mang giá trị 4 ) bằng tổng độ dài của DÃY CON TĂNG BÊN TRÁI dài nhất và DÃY CON GIẢM BÊN PHẢI dài nhất cộng thêm 1.
+- 3 + 1 + 1 = 5
+
+SỬ DỤNG THUẬT TOÁN : QUY HOẠCH ĐỘNG
+Cần thêm 2 mảng một chiều ( asc, des )
+- Mảng một chiều asc : mỗi phần tử thể hiện chiều dài của một dãy con tăng ( từ trái sang phải )
+- Mảng một chiều des : mỗi phần tử thể hiện chiều dài của một dãy con giảm ( từ phải sang trái )
+
+Bước 1 : Gán mảng asc, duyệt từ 0 -> n - 1
+    - Phần tử đầu tiên mang giá trị 1
+    - Phần tử thứ i : nếu asc[i] > asc[i - 1] thì asc[i] = asc[i - 1] + 1, ngược lại thì asc[i] = 1
+
+Bước 2 : Gán mảng des, duyệt từ n - 1 -> 0
+    - Phần tử thứ n - 1 mang giá trị 1
+    - Phần tử thứ i : nếu des[i] > des[i + 1] thì des[i] = des[i + 1] + 1, ngược lại thì des[i] = 1
+    
+Kết quả bài toán :
+    - Là một mảng có phần tử thứ i bằng tổng giá trị của asc[i], des[i] trừ đi 1
+
+Với input sau : 1 2 3 4 3 3 1
+Mảng asc là   : 1 2 3 4 1 1 1
+Mảng des là   : 1 1 1 2 1 2 1
+Đáp án là     : 1 2 3 5 1 2 1
+*/
+
 #include<stdio.h>
 
 int a[200000];
